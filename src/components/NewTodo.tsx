3a81ee2af662702e696e6b2129c40022587a9612
CodeSevenic <sevenicbookings@gmail.com>
2022-12-22 08:44:05 +0200

    React Typescript

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+
+const NewTodo: React.FC = () => {
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+  return (
+    <form>
+      <div>
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" />
+      </div>
+      <button type="submit">ADD TODO</button>
+    </form>
+  );
+};
+
+export default NewTodo;
